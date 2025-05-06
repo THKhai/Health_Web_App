d0d2689b31dcbc35e66ac1ffcad4125f89d27382
THKhai <116781770+THKhai@users.noreply.github.com>
2025-05-06 08:35:34 +0700

    Add: dbdown.bat

---
+++ WebApplication1/dbdown.bat
@@ -0,0 +1,14 @@
+@echo off
+echo "[STEP]: Stopping Docker containers..."
+docker-compose down
+if EXIST "Repository\data_base_log" (
+    echo "[STATUS]: Removing data_base_logs directory..."
+    rmdir /s /q "Repository\data_base_log"
+)
+
+if EXIST "Repository\data_log" (
+    echo "[STATUS]: Removing data_log directory..."
+    rmdir /s /q "Repository\data_log"
+)
+
+echo "[STATUS]: Successfully removed containers and data."
